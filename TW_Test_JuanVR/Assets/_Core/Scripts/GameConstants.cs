421adef8a44635de0e5f16cea58f1f96b602da0e
Juan Vazquez <diclonius32@gmail.com>
2021-12-01 12:47:18 -0600

    * [SplashScreen] Scene manager added.

---
+++ TW_Test_JuanVR/Assets/_Core/Scripts/GameConstants.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConstants : MonoBehaviour
+{
+    // SCENES
+
+    public static string S_SPLASH_SCREEN = "SplashScreen";
+    public static string S_GAME = "Game";
+}
